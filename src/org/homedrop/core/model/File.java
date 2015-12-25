04d0aee6b0a80fc3979b9ef3965f12391b4231c4
m87 <m87blackhole@gmail.com>
2015-12-25 20:24:29 +0100

    models

--- src/org/homedrop/core/model/File.java
+++ src/org/homedrop/core/model/File.java
@@ -1,4 +1,18 @@
 package org.homedrop.core.model;
 
+import java.sql.Date;
+import java.util.List;
+
 public class File {
+
+    private long id;
+    private String name;
+    private String path;
+    private List<Long> tags;
+    private long checkSum;
+    private long version;
+    private Date lastChange;
+    private long ownerId;
+    private List<Long> shareIds;
+
 }
---
+++ src/org/homedrop/core/model/Tag.java
@@ -0,0 +1,6 @@
+package org.homedrop.core.model;
+
+public class Tag {
+    private long id;
+    private String name;
+}
