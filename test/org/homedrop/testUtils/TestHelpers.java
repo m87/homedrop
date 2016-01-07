b59ed3e071d9d82dcfae1aecc329ec6827c004cd
ptasheq <piotr.t.ptaszynski@student.put.poznan.pl>
2016-01-07 10:40:21 +0100

    Unit test skeleton for hddb

--- test/org/homedrop/testUtils/TestHelpers.java
+++ test/org/homedrop/testUtils/TestHelpers.java
@@ -1,5 +1,10 @@
 package org.homedrop.testUtils;
 
 public class TestHelpers {
+
+    public static class HelperException extends Exception {
+        public HelperException() { super(); }
+    }
+
     public static final String MainConfigFilePath = "test-env/homedrop.cfg";
 }
--- test/org/homedrop/thirdParty/db/SqliteHDDBTest.java
+++ test/org/homedrop/thirdParty/db/SqliteHDDBTest.java
@@ -1,14 +1,59 @@
 package org.homedrop.thirdParty.db;
 
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.misc.TransactionManager;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import org.homedrop.core.model.User;
+import org.homedrop.manager.ConfigManager;
+import org.homedrop.manager.DependencyProvider;
+import org.homedrop.testUtils.TestHelpers;
+import org.homedrop.thirdParty.db.sqliteModels.UserEntity;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
 
 import static org.junit.Assert.*;
 
 public class SqliteHDDBTest {
 
+    DependencyProvider dependencyProvider;
+    SqliteHDDB sqliteHDDB;
+    JdbcConnectionSource connectionSource;
+
     @Before
     public void setUp() throws Exception {
+        ConfigManager config = ConfigManager.getInstance();
+        config.loadConfiguration("test-env/homedrop.cfg");
+        dependencyProvider = DependencyProvider.getInstance();
+        dependencyProvider.setConfig(config);
+        connectionSource = dependencyProvider.getDbConnectionSource();
+        sqliteHDDB = new SqliteHDDB(connectionSource);
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+        Class<?>[] entityClasses = { UserEntity.class };
+        for (Class<?> entityClass : entityClasses) {
+            TableUtils.dropTable(connectionSource, entityClass, true);
+        }
+        sqliteHDDB.onCreate();
+        try {
+            TransactionManager.callInTransaction(connectionSource, (Callable<Void>) () -> {
+                for (Class<?> entityClass : entityClasses) {
+                    TableUtils.dropTable(connectionSource, entityClass, false);
+                }
+                throw new TestHelpers.HelperException();
+            });
+        }
+        catch (SQLException e) {
+            Throwable causeException = e.getCause();
+            assertThat(causeException, instanceOf(TestHelpers.HelperException.class));
+        }
 
     }
 
